64e8288c01d35660625c8448bfe4175ec14e73db
Hrestonosec <egor.sheva2000@gmail.com>
2025-01-02 15:13:46 +0200

    Creating base objects: Debtor and TransactionsHistory class

---
+++ lib/models/debtor.dart
@@ -0,0 +1,7 @@
+class Debtor {
+  String name;
+  double debt;
+  int id;
+
+  Debtor(this.id, this.name, this.debt);
+}
---
+++ lib/models/transactions_history.dart
@@ -0,0 +1,6 @@
+class TransactionsHistory {
+  int id;
+  var transactionsList = Map<DateTime, double>;
+
+  TransactionsHistory(this.id, this.transactionsList);
+}
